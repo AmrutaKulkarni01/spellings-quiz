d97f98d9f2e696085bf0cbd0588b379770ff5a6d
Amruta Kulkarni <nm98wy9d@students.codeinstitute.net>
2024-09-15 14:32:07 +0000

    add formatting with beautify

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,46 @@
-let currentQuiz = "";  //this will hold name of the currently selected quiz 
+let currentQuiz = ""; //this will hold name of the currently selected quiz 
 let myArray; //this will hold names of the pictures in the current quiz-type       
 let index = -1; //index of the picture being currently displayed
 
 // Wait for the DOM to finish loading before running the game
 //Get the button elements and add event listeners to them
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByTagName("button");
-    
-    for (let button of buttons){
-        button.addEventListener("click", function() {
+
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
             if (this.id == "backspace") {
                 console.log("you clicked backspace");
                 deleteLastChar();
-            }
-            else if (this.id == "next") {
+            } else if (this.id == "next") {
                 //check for emptiness of array
-                if(typeof myArray === 'undefined' || myArray.length < 1) {
+                if (typeof myArray === 'undefined' || myArray.length < 1) {
                     alert("Please select a new Quiz!");
-                }                
-                else {
+                } else {
                     // Get random number between 0 and max index of the array                
                     index = Math.floor(Math.random() * myArray.length);
-                    displayNextImage(myArray[index]);                
+                    displayNextImage(myArray[index]);
                 }
-            }
-            else if(this.id == "submit") {
+            } else if (this.id == "submit") {
                 console.log(myArray);
                 //check for emptiness of array
-                if(typeof myArray === 'undefined' || myArray.length < 1) {
+                if (typeof myArray === 'undefined' || myArray.length < 1) {
                     alert("Please select a new Quiz!");
+                } else {
+                    checkSpelling();
                 }
-                else{
-                    checkSpelling();                    
-                }                                
-            }
-            else if (this.classList.contains("alpha")) {                
-                if(currentQuiz === ""){
+            } else if (this.classList.contains("alpha")) {
+                if (currentQuiz === "") {
                     alert("Please select a new Quiz!");
+                } else {
+                    document.getElementById("spelling").value += this.textContent;
                 }
-                else{
-                    document.getElementById("spelling").value += this.textContent;             
-                }
-            }
-            else if (this.className === "quiz-type") {               
+            } else if (this.className === "quiz-type") {
                 changeCurrentQuiz(this.textContent);
                 startNewQuiz();
             }
-        }) 
-    }    
+        })
+    }
 })
 
 /**
@@ -64,42 +57,39 @@ function changeCurrentQuiz(newQuiz) {
             btncurrQuiz.style.color = "black";
         }
     }
-    
+
     currentQuiz = newQuiz;
-    if(newQuiz == "")
+    if (newQuiz == "")
         return;
     //change color of new quiz button
-    var btnQuiz =  document.getElementById(currentQuiz);
+    var btnQuiz = document.getElementById(currentQuiz);
     if (btnQuiz !== null) {
         btnQuiz.style.backgroundColor = "green";
-        btnQuiz.style.color = "white";    
-    }    
+        btnQuiz.style.color = "white";
+    }
 }
 
 /**
  * This function is called when user clicks any of the 4 quiz-type buttons,
  * with an intention of starting a new quiz.
  */
-function startNewQuiz() {    
+function startNewQuiz() {
     //clear score cards
     document.getElementById("correct").textContent = 0;
     document.getElementById("incorrect").textContent = 0;
     // get currentQuiz and change color of the respective button
-    console.log("Inside startNewQuiz : currentQuiz : "+currentQuiz);
+    console.log("Inside startNewQuiz : currentQuiz : " + currentQuiz);
     //let myArray;
-    if ( currentQuiz === "Fruits") {
-        myArray = ["apple", "mango", "banana", "grapes", "strawberry" ];  
-    }
-    else if (currentQuiz === "Colors") {
+    if (currentQuiz === "Fruits") {
+        myArray = ["apple", "mango", "banana", "grapes", "strawberry"];
+    } else if (currentQuiz === "Colors") {
         myArray = []; // ["red", "yellow", "blue", "green", "orange" ];  
+    } else if (currentQuiz === "Animals") {
+        myArray = ["tiger", "lion", "fox", "giraffe", "turtle", "monkey", "zebra"];
+    } else if (currentQuiz === "Birds") {
+        myArray = ["crow", "sparrow", "parrot", "peacock", "hen"];
     }
-    else if (currentQuiz === "Animals") {
-        myArray = ["tiger", "lion", "fox", "giraffe", "turtle", "monkey", "zebra" ];  
-    }
-    else if (currentQuiz === "Birds") {
-        myArray = ["crow", "sparrow", "parrot", "peacock", "hen" ];  
-    }
-        
+
     // Get random number between 0 and max index of the array
     if (myArray.length > 0) {
         index = Math.floor(Math.random() * myArray.length);
@@ -116,10 +106,10 @@ function displayNextImage(name) {
     document.getElementById("spelling").value = "";
 
     //display next picture
-    console.log("Inside displayNextImage : "+name);
+    console.log("Inside displayNextImage : " + name);
     let imgElement = document.getElementById("display-pic");
     let imgPath = "assets/images/" + name + ".png";
-    imgElement.setAttribute("src", imgPath);    
+    imgElement.setAttribute("src", imgPath);
 }
 
 /**
@@ -130,7 +120,7 @@ function deleteLastChar() {
     let textBox = document.getElementById("spelling");
     if (textBox.value != "") {
         let strValue = textBox.value;
-        strValue = strValue.substr(0, strValue.length-1);
+        strValue = strValue.substr(0, strValue.length - 1);
         textBox.value = strValue;
     }
 }
@@ -142,10 +132,9 @@ function checkSpelling() {
     let textBox = document.getElementById("spelling")
     if (textBox.value != "") {
         let strValue = textBox.value.toLowerCase();
-        if (strValue === myArray[index]) {            
+        if (strValue === myArray[index]) {
             incrementScore();
-        }
-        else {            
+        } else {
             incrementIncorrectScore();
         }
     }
@@ -157,22 +146,19 @@ function checkSpelling() {
     if (myArray.length > 0) {
         index = Math.floor(Math.random() * myArray.length);
         displayNextImage(myArray[index]);
-    }
-    else {
+    } else {
         let score = parseInt(document.getElementById("correct").textContent);
         //deselect current quiz
         changeCurrentQuiz("");
         alert(`Final Score : ${score}\nPlease select a new Quiz!`);
     }
-
-    
 }
 
 /**
  * This function will increment and display the correct score
  */
 function incrementScore() {
-    let elementScore = document.getElementById("correct");  
+    let elementScore = document.getElementById("correct");
     let score = parseInt(elementScore.textContent);
     elementScore.textContent = ++score;
 }
@@ -181,7 +167,7 @@ function incrementScore() {
  * This function will increment and display the incorrect score
  */
 function incrementIncorrectScore() {
-    let elementIncorrectScore = document.getElementById("incorrect");    
+    let elementIncorrectScore = document.getElementById("incorrect");
     let score = parseInt(elementIncorrectScore.textContent);
     elementIncorrectScore.textContent = ++score;
 }
